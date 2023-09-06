b2ac0c634f5680be8faa086a8b18da7e8adf56c2
EC2 Default User <ec2-user@ip-172-31-44-51.ap-northeast-1.compute.internal>
2023-09-06 15:33:11 +0000

    [Add] devise以外のコントローラ作成,管理者ログイン機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,10 @@ Rails.application.routes.draw do
     sessions: "admin/sessions"
   }
 
+  namespace :admin do
+    root 'homes#top'
+  end
+
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

53ad8355676d07f5ca3accee8599cb3b2fd6ef4f
EC2 Default User <ec2-user@ip-172-31-44-51.ap-northeast-1.compute.internal>
2023-09-27 17:26:46 +0000

    [Add] 管理者注文詳細

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,4 +1,9 @@
 class Admin::OrdersController < ApplicationController
+  before_action :authenticate_admin!
+
   def show
+    @order = Order.find(params[:id])
+    @item = Item.find(params[:id])
+    @order_details = @order.order_details.all
   end
 end
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,4 +1,8 @@
 class OrderDetail < ApplicationRecord
   belongs_to :order
   belongs_to :item
+
+  def subtotal
+    tax_price * amount
+  end
 end
